998e2e2b86796697073ec34b99dda69ad9423b2b
Michael Reyes <mradianlogs@gmail.com>
2020-03-05 01:59:23 -0600

    Working on getting NPCs to move on grid.

--- Assets/Scripts/SimGridMap.cs
+++ Assets/Scripts/SimGridMap.cs
@@ -10,20 +10,24 @@ public class SimGridMap : MonoBehaviour
     private int mapSize; //How many cells (mapsize x mapsize) are in the map.
     [SerializeField]
     private int cellSize; //How big each cell should be, meaning how much space it represents in the map.
-    [SerializeField]
     private float cellSizeOffset; //The offset to get to the middle of the cell. Half the cellSize.
 
     [SerializeField]
-    private Transform mapZeroZeroPosition; //The top left position of the map.
+    private Transform mapZeroZeroPosition; //The top left position of the map. The x position is the col, z is the row.
 
     [SerializeField]
-    private GameObject testObject;
+    private GameObject testObjectPrefab;
+    [SerializeField]
+    private GameObject testNPCPrefab;
+    private GameObject testNPC;
 
     // Start is called before the first frame update
     void Start()
     {
         mapGrid = new int[mapSize][];
+        cellSizeOffset = (float)cellSize / 2;
 
+        /*
         //Testing cell placements.
         for (int i = 0; i < mapSize; i++)
         {
@@ -32,11 +36,47 @@ public class SimGridMap : MonoBehaviour
                 Instantiate(testObject, new Vector3((mapZeroZeroPosition.position.x + ((cellSize * j) + cellSizeOffset)), 0, (mapZeroZeroPosition.position.z - ((cellSize * i) + cellSizeOffset))), mapZeroZeroPosition.rotation);
             }
         }
+        
+        //Instantiate(testObject, new Vector3((mapZeroZeroPosition.position.x + ((cellSize * 0) + cellSizeOffset)), 0, (mapZeroZeroPosition.position.z - ((cellSize * 0) + cellSizeOffset))), mapZeroZeroPosition.rotation);
+        //Instantiate(testObject, GetCellMapPosition(0,1), mapZeroZeroPosition.rotation);
+        InsertObjectInMapAtPosition(testObject, 0,0);
+        InsertObjectInMapAtPosition(testObject, 1, 2);
+        */
+
+        testNPC = InsertReturnObjectInMapAtPosition(testNPCPrefab, 1, 2);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Time.fixedTime == 5f)
+        {
+            Debug.Log("Asking NPC to move.");
+            testNPC.GetComponent<SimNPC>().ShiftPositionUp();
+        }
         
+        
+        
+    }
+
+    public int GetCellSize()
+    {
+        return cellSize;
+    }
+
+    public Vector3 GetCellMapPosition(int row, int col)
+    {
+        return new Vector3((mapZeroZeroPosition.position.x + ((cellSize * col) + cellSizeOffset)), 0, (mapZeroZeroPosition.position.z - ((cellSize * row) + cellSizeOffset)));
+    }
+
+    public void InsertObjectInMapAtPosition(GameObject newObject, int row, int col)
+    {
+        Instantiate(newObject, GetCellMapPosition(row, col), mapZeroZeroPosition.rotation);
+    }
+
+    public GameObject InsertReturnObjectInMapAtPosition(GameObject newObject, int row, int col)
+    {
+        return Instantiate(newObject, GetCellMapPosition(row, col), mapZeroZeroPosition.rotation);
     }
 }
--- Assets/Scripts/SimNPC.cs
+++ Assets/Scripts/SimNPC.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class SimNPC : MonoBehaviour
 {
-    private bool isBusy; //Whether this NPC is assigned to do something.
+    //[SerializeField]
+    private SimGridMap map;
+    [SerializeField]
+    private float cellSize;
 
+    private bool isBusy; //Whether this NPC is assigned to do something.
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        map = GameObject.FindGameObjectWithTag("SimMap").GetComponent<SimGridMap>();
+        cellSize = map.GetCellSize();
     }
 
     // Update is called once per frame
@@ -32,5 +37,30 @@ public class SimNPC : MonoBehaviour
     public bool IsBusy()
     {
         return isBusy;
+    }
+
+
+    //Change to moving towards position over time! A star will be repeating those methods, following best path, until at end!
+    public void ShiftPositionUp()
+    {
+        //Use rigidbody translate? To move npc to new position? Or just changed position?
+        //transform.Translate(0,0, cellSize, Space.World);
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + cellSize);
+        Debug.Log("NPC shifted up!");
+    }
+
+    public void ShiftPositionDown()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - cellSize);
+    }
+
+    public void ShiftPositionLeft()
+    {
+        transform.position = new Vector3(transform.position.x - cellSize, transform.position.y, transform.position.z);
+    }
+
+    public void ShiftPositionRight()
+    {
+        transform.position = new Vector3(transform.position.x + cellSize, transform.position.y, transform.position.z + cellSize);
     }
 }
